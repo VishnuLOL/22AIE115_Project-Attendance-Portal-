77f7595f5cfc84b63560df2f8e271cfbe705ae11
Vishnu J <vishnujr106@gmail.com>
2024-05-19 16:59:20 +0530

    Second commit

---
+++ Student-Dashboard-main/timeTable.js
@@ -0,0 +1,133 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'N101',
+        subject: 'UID',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: 'A201',
+        subject: 'Computational Maths',
+        type: 'Tutorial'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'N203',
+        subject: 'OOPS',
+        type: 'Lecture'
+    }
+]
+const Tuesday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'N101',
+        subject: 'UID',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: 'A201',
+        subject: 'Computational Maths',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'N203',
+        subject: 'OOPS',
+        type: 'Lecture'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '10-12 AM',
+        roomNumber: 'N201',
+        subject: 'Computational Maths',
+        type: 'Lab'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: 'N203',
+        subject: 'UID',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'A203',
+        subject: 'EEE',
+        type: 'Lecture'
+    },
+]
+
+const Thursday =[
+    {   
+        time: '11-12 AM',
+        roomNumber: 'N201',
+        subject: 'OOPS',
+        type: 'Lecture'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: 'A203',
+        subject: 'EEE',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '38-718',
+        subject: 'DSA',
+        type: 'Lecture'
+    }
+]
+
+const Friday =[
+    {   
+        time: '10-12 AM',
+        roomNumber: 'N201',
+        subject: 'DSA',
+        type: 'Lab'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: 'N203',
+        subject: 'UID',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'A203',
+        subject: 'EEE',
+        type: 'Lecture'
+    },
+
+]
+
+const Saturday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'N101',
+        subject: 'UID',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: 'A201',
+        subject: 'Computational Maths',
+        type: 'Tutorial'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'N203',
+        subject: 'OOPS',
+        type: 'Lecture'
+    }
+]
---
+++ Teacher-Dashboard-main/timeTable.js
@@ -0,0 +1,133 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        Batch: 'No class AvaiS2le',
+        Semester: ''
+    }
+]
+const Monday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'N101',
+        Batch: 'AIE-B',
+        Semester: 'S2'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: 'A201',
+        Batch: 'AIE-A',
+        Semester: 'S4'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'N203',
+        Batch: 'CSE-B',
+        Semester: 'S2'
+    }
+]
+const Tuesday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'N101',
+        Batch: 'AIE-A',
+        Semester: 'S2'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: 'A201',
+        Batch: 'CSE-A',
+        Semester: 'S6'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'N203',
+        Batch: 'CSE-C',
+        Semester: 'S4'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '10-12 AM',
+        roomNumber: 'N201',
+        Batch: 'CSE-A',
+        Semester: 'S2'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: 'N203',
+        Batch: 'AIE-B',
+        Semester: 'S6'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'A203',
+        Batch: 'AIE-A',
+        Semester: 'S2'
+    },
+]
+
+const Thursday =[
+    {   
+        time: '11-12 AM',
+        roomNumber: 'N201',
+        Batch: 'CSE-C',
+        Semester: 'S6'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: 'A203',
+        Batch: 'AIE-A',
+        Semester: 'S6'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '38-718',
+        Batch: 'CSE-B',
+        Semester: 'S6'
+    }
+]
+
+const Friday =[
+    {   
+        time: '10-12 AM',
+        roomNumber: 'N201',
+        Batch: 'CSE-B',
+        Semester: 'S2'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: 'N203',
+        Batch: 'AIE-B',
+        Semester: 'S6'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'A203',
+        Batch: 'AIE-A',
+        Semester: 'S6'
+    },
+
+]
+
+const Saturday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'N101',
+        Batch: 'AIE-B',
+        Semester: 'S4'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: 'A201',
+        Batch: 'CSE-A',
+        Semester: 'S2'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'N203',
+        Batch: 'CSE-C',
+        Semester: 'S6'
+    }
+]
